9803a837a35564cf50169f31a797f9178d407bb4
fentontaylor <fentontaylor@gmail.com>
2020-01-22 15:10:24 -0700

    Update models to allow null for github and production links

--- projects/models.py
+++ projects/models.py
@@ -12,10 +12,10 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     language = models.CharField(max_length=20)
-    technologies = models.ManyToManyField('Technology', related_name='projects', blank=True, default='')
+    technologies = models.ManyToManyField('Technology', related_name='projects', blank=True)
     image = models.FilePathField(path='/img')
-    github = models.URLField(max_length=200, blank=True, default='')
-    production = models.URLField(max_length=200, blank=True, default='')
+    github = models.URLField(max_length=200, blank=True, null=True)
+    production = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         desc = f'{self.description[0:7]}...'

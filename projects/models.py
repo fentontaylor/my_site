7c5074118d08b443ddb1794a12c91a202110693b
fentontaylor <fentontaylor@gmail.com>
2020-01-22 09:29:18 -0700

    Create migration for projects and technologies

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
+
+class Technology(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    language = models.CharField(max_length=20)
+    technologies = models.ManyToManyField('Technology', related_name='projects', blank=True, default='')
     image = models.FilePathField(path='/img')
+    github = models.URLField(max_length=200, blank=True, default='')
+    production = models.URLField(max_length=200, blank=True, default='')
+
+    def __str__(self):
+        return self.title

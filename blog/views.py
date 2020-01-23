cb7563aa3eb99555c9555cef565d2229969135d2
fentontaylor <fentontaylor@gmail.com>
2020-01-23 11:43:50 -0700

    Add CommentForm to forms.py and add to blog_show()

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 
 def blog_index(request):
@@ -23,6 +24,18 @@ def blog_category(request, category):
 
 def blog_show(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            comment.save()
+            
     comments = Comment.objects.filter(post=post)
     context = {
         'post': post,

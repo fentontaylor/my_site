448cec23662345f3b8b8731a74cc48629e435107
fentontaylor <fentontaylor@gmail.com>
2020-01-23 12:25:16 -0700

    Add blog_category.html template

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def blog_index(request):
 
 def blog_category(request, category):
     posts = Post.objects.filter(
-        categories_name_contains=category
+        categories__name__contains=category
     ).order_by(
         '-created_on'
     )

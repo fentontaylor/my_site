4b1dc781eb84d3bcef8f16c71acabf04cdbf145b
fentontaylor <fentontaylor@gmail.com>
2020-01-23 17:06:03 -0700

    Add allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import socket
 import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.fentontaylor.com']
 
 
 # Application definition
